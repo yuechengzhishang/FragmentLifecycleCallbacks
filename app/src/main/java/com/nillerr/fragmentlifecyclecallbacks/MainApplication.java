eafbef9177ee1f5f5006b42c7c9dc6567129d655
Nicklas Jensen <nillerr@gmail.com>
2015-04-12 13:53:58 +0200

    Removed comment

--- app/src/main/java/com/nillerr/fragmentlifecyclecallbacks/MainApplication.java
+++ app/src/main/java/com/nillerr/fragmentlifecyclecallbacks/MainApplication.java
@@ -2,9 +2,6 @@ package com.nillerr.fragmentlifecyclecallbacks;
 
 import android.app.Application;
 
-/**
- * Created by nillerr on 12/04/15.
- */
 public class MainApplication extends Application {
 
     @Override
